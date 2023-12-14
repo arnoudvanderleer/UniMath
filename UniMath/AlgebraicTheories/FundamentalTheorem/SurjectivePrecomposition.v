d1d800479d6256eeab1000d56be247dc3f0291dd
Tempestas Ludi <tempestasludi@gmail.com>
2023-12-14 17:46:37 +0100

    Reorganize the proof of precomposition with a fully faithful functor

--- UniMath/AlgebraicTheories/FundamentalTheorem/SurjectivePrecomposition.v
+++ UniMath/AlgebraicTheories/FundamentalTheorem/SurjectivePrecomposition.v
@@ -25,12 +25,12 @@ Section PrecompositionEssentiallySurjective.
 
   Section Iso.
 
-    Context (P : [C, D]).
+    Context (P : C ⟶ D).
+    Context (c : C).
 
-    Definition pre_comp_z_iso_mor_data
-      : nat_trans_data (pre_comp_functor F (lan_functor HD F P) : C ⟶ D) (P : C ⟶ D).
+    Definition pre_comp_z_iso_inv
+      : D⟦(lan_functor HD F P : C' ⟶ D) (F c), P c⟧.
     Proof.
-      intro c.
       use (colimArrow _ _ (make_cocone _ _)).
       - intros v.
         exact (#(P : _ ⟶ _) (invmap (Fweq _ _) (pr2 v))).
@@ -45,34 +45,18 @@ Section PrecompositionEssentiallySurjective.
         ).
     Defined.
 
-    Lemma pre_comp_z_iso_mor_is_nat_trans
-      : is_nat_trans _ _ pre_comp_z_iso_mor_data.
-    Proof.
-      intros c c' f.
-      apply colim_mor_eq.
-      intro v.
-      do 2 refine (assoc _ _ _ @ !_).
-      refine (maponpaths (λ x, x · _) (lan_mor_colimIn _ _ _ _ _ (pr2 v) _) @ !_).
-      refine (maponpaths (λ x, x · _) (colimArrowCommutes _ _ _ v) @ !_).
-      refine (colimArrowCommutes (lan_colim _ _ _ _) _ _ ((pr11 v ,, _) ,, pr2 v · #F f) @ _).
-      refine (_ @ functor_comp P _ _).
-      apply (maponpaths #(P : _ ⟶ _)).
-      apply invmap_eq.
-      refine (_ @ !functor_comp F _ _).
-      exact (!maponpaths (λ x, x · _) (homotweqinvweq (Fweq _ _) _)).
-    Qed.
-
-    Definition pre_comp_z_iso_mor
-      : nat_trans (pre_comp_functor F (lan_functor HD F P) : C ⟶ D) (P : C ⟶ D)
-      := make_nat_trans _ _ _ pre_comp_z_iso_mor_is_nat_trans.
-
-    Lemma pre_comp_z_iso_is_iso
-      : is_inverse_in_precat (C := [C, D]) pre_comp_z_iso_mor (unit_from_right_adjoint (is_right_adjoint_precomposition HD F) P).
+    Lemma pre_comp_z_iso_is_inverse
+      : is_inverse_in_precat (C := D)
+        ((unit_from_right_adjoint (is_right_adjoint_precomposition HD F) P : _ ⟹ _) c)
+        pre_comp_z_iso_inv.
     Proof.
       split.
-      - apply nat_trans_eq_alt.
-        intro c.
-        apply colim_mor_eq.
+      - refine (_ @ functor_id P _).
+        refine (colimArrowCommutes (lan_colim HD F P (F c)) _ _ ((c ,, tt) ,, identity (F c)) @ _).
+        refine (maponpaths (λ f, # (P : _ ⟶ _) f) (_ : invmap (Fweq c c) _ = _)).
+        apply invmap_eq.
+        exact (!functor_id F _).
+      - apply colim_mor_eq.
         intro v.
         refine (assoc _ _ _ @ _).
         match goal with
@@ -83,23 +67,21 @@ Section PrecompositionEssentiallySurjective.
         + refine (maponpaths (λ x, x · _) _).
           exact (!homotweqinvweq (Fweq _ _) _).
         + exact (!id_right _).
-      - apply nat_trans_eq_alt.
-        intro c.
-        refine (_ @ functor_id P _).
-        refine (colimArrowCommutes (lan_colim HD F P (F c)) _ _ ((c ,, tt) ,, identity (F c)) @ _).
-        refine (maponpaths (λ f, # (P : _ ⟶ _) f) (_ : invmap (Fweq c c) _ = _)).
-        apply invmap_eq.
-        exact (!functor_id F _).
     Qed.
 
-    Definition pre_comp_after_lan_iso
-      : z_iso (pre_comp_functor F (lan_functor HD F P)) P
-      := make_z_iso _ _ pre_comp_z_iso_is_iso.
+    Definition pre_comp_is_z_iso
+      : is_z_isomorphism ((unit_from_right_adjoint (is_right_adjoint_precomposition HD F) P : _ ⟹ _) c)
+      := make_is_z_isomorphism _ _ pre_comp_z_iso_is_inverse.
 
   End Iso.
 
+  Definition pre_comp_after_lan_iso
+    (P : [C, D])
+    : z_iso P (pre_comp_functor F (lan_functor HD F P))
+    := z_iso_from_nat_z_iso _ (make_nat_z_iso _ _ _ (pre_comp_is_z_iso _)).
+
   Definition pre_comp_split_essentially_surjective
     : split_essentially_surjective (pre_comp_functor F)
-    := λ c, lan_functor HD F c ,, pre_comp_after_lan_iso c.
+    := λ c, lan_functor HD F c ,, (z_iso_inv (pre_comp_after_lan_iso c)).
 
 End PrecompositionEssentiallySurjective.
