4924b24a31ed70884435272df710f4fda14939cb
Tempestas Ludi <tempestasludi@gmail.com>
2023-12-14 16:45:13 +0100

    Generalize equivalence of the karoubi envelope presheaves to arbitrary functors

---
+++ UniMath/AlgebraicTheories/FundamentalTheorem/CommonUtilities/KaroubiEnvelope.v
@@ -0,0 +1,340 @@
+Require Import UniMath.Foundations.All.
+Require Import UniMath.MoreFoundations.All.
+Require Import UniMath.CategoryTheory.Adjunctions.Core.
+Require Import UniMath.CategoryTheory.categories.HSET.Core.
+Require Import UniMath.CategoryTheory.categories.HSET.Univalence.
+Require Import UniMath.CategoryTheory.Core.Prelude.
+Require Import UniMath.CategoryTheory.Equivalences.Core.
+Require Import UniMath.CategoryTheory.FunctorCategory.
+Require Import UniMath.CategoryTheory.LeftKanExtension.
+Require Import UniMath.CategoryTheory.limits.graphs.colimits.
+Require Import UniMath.CategoryTheory.opp_precat.
+Require Import UniMath.CategoryTheory.Presheaf.
+Require Import UniMath.CategoryTheory.whiskering.
+
+Require Import UniMath.AlgebraicTheories.FundamentalTheorem.SurjectivePrecomposition.
+
+Local Open Scope cat.
+
+Section KaroubiEnvelope.
+
+  Context (C : category).
+
+  Definition karoubi_envelope_data
+    : precategory_data.
+  Proof.
+    use make_precategory_data.
+    - use make_precategory_ob_mor.
+      + exact (∑ (c : C) (f : C⟦c, c⟧), f · f = f).
+      + intros f f'.
+        exact (∑ (g : C⟦pr1 f, pr1 f'⟧), pr12 f · g = g × g · pr12 f' = g).
+    - intro f.
+      repeat use tpair.
+      + exact (pr12 f).
+      + exact (pr22 f).
+      + exact (pr22 f).
+    - intros f f' f'' g g'.
+      repeat use tpair.
+      + exact (pr1 g · pr1 g').
+      + abstract now rewrite assoc, (pr12 g).
+      + abstract now rewrite assoc', (pr22 g').
+  Defined.
+
+  Definition karoubi_mor_eq
+    {a b : karoubi_envelope_data}
+    (f g : karoubi_envelope_data⟦a, b⟧)
+    (H : pr1 f = pr1 g)
+    : f = g.
+  Proof.
+    apply subtypePath.
+    {
+      intro c.
+      apply isapropdirprod;
+        apply homset_property.
+    }
+    exact H.
+  Qed.
+
+  Lemma karoubi_envelope_is_precategory
+    : is_precategory karoubi_envelope_data.
+  Proof.
+    apply make_is_precategory_one_assoc.
+    - intros m n f.
+      apply karoubi_mor_eq.
+      exact (pr12 f).
+    - intros m n f.
+      apply karoubi_mor_eq.
+      exact (pr22 f).
+    - intros k l m n f g h.
+      apply karoubi_mor_eq.
+      apply assoc.
+  Qed.
+
+  Lemma karoubi_envelope_has_homsets
+    : has_homsets karoubi_envelope_data.
+  Proof.
+    intros m n.
+    apply isaset_total2.
+    - apply homset_property.
+    - intro x.
+      apply isasetaprop.
+      apply isapropdirprod;
+        apply homset_property.
+  Qed.
+
+  Definition karoubi_envelope
+    : category
+    := make_category
+      (make_precategory
+        karoubi_envelope_data
+        karoubi_envelope_is_precategory)
+      karoubi_envelope_has_homsets.
+
+  Definition karoubi_envelope_inclusion
+    : C ⟶ karoubi_envelope.
+  Proof.
+    use make_functor.
+    - use make_functor_data.
+      + intro c.
+        repeat use tpair.
+        * exact c.
+        * apply identity.
+        * apply id_left.
+      + intros a b f.
+        repeat use tpair.
+        * exact f.
+        * apply id_left.
+        * apply id_right.
+    - split.
+      + abstract (
+          intro a;
+          now apply karoubi_mor_eq
+        ).
+      + abstract (
+          intros a b c f g;
+          now apply karoubi_mor_eq
+        ).
+  Defined.
+
+  Lemma karoubi_envelope_inclusion_fully_faithful
+    : fully_faithful karoubi_envelope_inclusion.
+  Proof.
+    intros c c'.
+    use isweq_iso.
+    - intro f.
+      exact (pr1 f).
+    - apply idpath.
+    - intro f.
+      now apply karoubi_mor_eq.
+  Qed.
+
+  Context (D : category).
+  Context (HD : Colims D).
+
+  Section Iso.
+
+    Context (P : karoubi_envelope ⟶ D).
+    Context (c : karoubi_envelope).
+
+    Definition karoubi_functor_iso_inv
+      : D ⟦ P c, lan_point HD karoubi_envelope_inclusion (functor_compose karoubi_envelope_inclusion P) c⟧.
+    Proof.
+      refine (_ · colimIn _ (((pr1 c ,, tt) ,, pr12 c ,, id_left _ ,, pr22 c) : (vertex (lan_comma karoubi_envelope_inclusion c)))).
+      apply (#P).
+      exact (pr12 c ,, pr22 c ,, id_right _).
+    Defined.
+
+    Lemma karoubi_functor_iso_is_inverse
+      : is_inverse_in_precat
+        (pr1 (Core.counit_from_right_adjoint (is_right_adjoint_precomposition HD karoubi_envelope_inclusion) P) c)
+        karoubi_functor_iso_inv.
+    Proof.
+      split.
+      - refine (assoc _ _ _ @ _).
+        apply colim_mor_eq.
+        intro v.
+        refine (assoc _ _ _ @ _).
+        refine (maponpaths (λ x, x · _) (assoc _ _ _) @ _).
+        refine (maponpaths (λ x, x · _ · _) (lan_precomposition_counit_point_colimIn HD _ P c _ _ _) @ _).
+        refine (_ @ !id_right _).
+        refine (!maponpaths (λ x, x · _) (functor_comp P _ _) @ _).
+        use (maponpaths (λ x, #P x · _) _
+          @ colimInCommutes
+            (lan_colim HD karoubi_envelope_inclusion (pre_comp_functor karoubi_envelope_inclusion P) c)
+            v
+            ((pr1 c ,, tt) ,, pr12 c ,, id_left _ ,, pr22 c)
+            ((pr12 v ,, (pr2 iscontrunit _)) ,, _));
+        apply karoubi_mor_eq.
+        + exact (pr222 v).
+        + apply idpath.
+      - refine (assoc' _ _ _ @ _).
+        refine (maponpaths _ (lan_precomposition_counit_point_colimIn HD _ P c _ _ _) @ _).
+        refine (!functor_comp _ _ _ @ _).
+        refine (_ @ functor_id P _).
+        apply maponpaths.
+        apply karoubi_mor_eq.
+        exact (pr22 c).
+    Qed.
+
+  End Iso.
+
+  Definition karoubi_functor_iso
+    (P : karoubi_envelope ⟶ D)
+    : is_z_isomorphism (Core.counit_from_right_adjoint (is_right_adjoint_precomposition HD karoubi_envelope_inclusion) P)
+    := nat_trafo_z_iso_if_pointwise_z_iso _ _
+      (λ c, (make_is_z_isomorphism _ _ (karoubi_functor_iso_is_inverse P c))).
+
+  Definition karoubi_pullback_equivalence
+    : adj_equivalence_of_cats
+      (pre_comp_functor (C := D) karoubi_envelope_inclusion).
+  Proof.
+    use adj_equivalence_from_right_adjoint.
+    - apply (is_right_adjoint_precomposition HD).
+    - intro P.
+      exact (z_iso_is_z_isomorphism (pre_comp_after_lan_iso _ karoubi_envelope_inclusion_fully_faithful _ HD P)).
+    - exact karoubi_functor_iso.
+  Defined.
+
+End KaroubiEnvelope.
+
+Section OppKaroubiEquiv.
+
+  Context (C : category).
+
+  Definition opp_karoubi_functor_data
+    : functor_data (op_cat (karoubi_envelope C)) (karoubi_envelope (op_cat C)).
+  Proof.
+    use make_functor_data.
+    - exact (idfun _).
+    - exact (λ a b f, pr1 f ,, pr22 f ,, pr12 f).
+  Defined.
+
+  Lemma opp_karoubi_functor_is_functor
+    : is_functor opp_karoubi_functor_data.
+  Proof.
+    split.
+    - easy.
+    - intros a b c f g.
+      now apply karoubi_mor_eq.
+  Qed.
+
+  Definition opp_karoubi_functor
+    : op_cat (karoubi_envelope C) ⟶ karoubi_envelope (op_cat C)
+    := make_functor _ opp_karoubi_functor_is_functor.
+
+  Definition opp_karoubi_ob_lift
+    : karoubi_envelope (op_cat C) → op_cat (karoubi_envelope C)
+    := idfun _.
+
+  Definition opp_karoubi_ob_lift_mor
+    (c : karoubi_envelope (op_cat C))
+    : karoubi_envelope (op_cat C) ⟦ opp_karoubi_functor (opp_karoubi_ob_lift c), c ⟧
+    := pr12 c ,, pr22 c ,, pr22 c.
+
+  Section OppKaroubiOfLiftIsUniversalArrow.
+
+    Context (c : karoubi_envelope (op_cat C)).
+    Context (c': op_cat (karoubi_envelope C)).
+    Context (f: karoubi_envelope (op_cat C) ⟦ opp_karoubi_functor c', c ⟧).
+
+    Definition opp_karoubi_universal_mor
+      : op_cat (karoubi_envelope C) ⟦ c', opp_karoubi_ob_lift c ⟧
+      := pr1 f ,, pr22 f ,, pr12 f.
+
+    Lemma opp_karoubi_universal_eq
+      : f = # opp_karoubi_functor opp_karoubi_universal_mor · opp_karoubi_ob_lift_mor c.
+    Proof.
+      apply karoubi_mor_eq.
+      exact (!pr22 f).
+    Qed.
+
+    Lemma opp_karoubi_universal_prop
+      (g : op_cat (karoubi_envelope C) ⟦ c', opp_karoubi_ob_lift c ⟧)
+      : isaprop (f = # opp_karoubi_functor g · opp_karoubi_ob_lift_mor c).
+    Proof.
+      apply homset_property.
+    Qed.
+
+    Lemma opp_karoubi_universal_eq'
+      (g: op_cat (karoubi_envelope C) ⟦ c', opp_karoubi_ob_lift c ⟧)
+      (H: f = # opp_karoubi_functor g · opp_karoubi_ob_lift_mor c)
+      : g = opp_karoubi_universal_mor.
+    Proof.
+      apply karoubi_mor_eq.
+      refine (_ @ !base_paths _ _ H).
+      exact (!pr12 g).
+    Qed.
+
+    Definition opp_karoubi_universal
+      : ∃! f', f = # opp_karoubi_functor f' · opp_karoubi_ob_lift_mor c
+      := unique_exists
+        opp_karoubi_universal_mor
+        opp_karoubi_universal_eq
+        opp_karoubi_universal_prop
+        opp_karoubi_universal_eq'.
+
+  End OppKaroubiOfLiftIsUniversalArrow.
+
+  Definition opp_karoubi_is_adjoint
+    : is_left_adjoint opp_karoubi_functor
+    := left_adjoint_from_partial _
+      opp_karoubi_ob_lift
+      opp_karoubi_ob_lift_mor
+      opp_karoubi_universal.
+
+  Section OppKaroubiUnitIso.
+
+    Context (c : op_cat (karoubi_envelope C)).
+
+    Definition opp_karoubi_unit_iso_inv
+      : karoubi_envelope C ⟦ c, c ⟧
+      := pr12 c ,, pr22 c ,, pr22 c.
+
+    Lemma opp_karoubi_unit_is_inverse
+      : is_inverse_in_precat (adjunit opp_karoubi_is_adjoint c) opp_karoubi_unit_iso_inv.
+    Proof.
+      split.
+      - apply karoubi_mor_eq.
+        exact (pr22 c).
+      - apply karoubi_mor_eq.
+        exact (pr22 c).
+    Qed.
+
+    Definition opp_karoubi_unit_is_iso
+      : is_z_isomorphism (adjunit opp_karoubi_is_adjoint c)
+      := make_is_z_isomorphism _ _ opp_karoubi_unit_is_inverse.
+
+  End OppKaroubiUnitIso.
+
+  Section OppKaroubiCounitIso.
+
+    Context (c : karoubi_envelope (op_cat C)).
+
+    Definition opp_karoubi_counit_iso_inv
+      : karoubi_envelope (op_cat C) ⟦ c, opp_karoubi_ob_lift c ⟧
+      := pr12 c ,, pr22 c ,, pr22 c.
+
+    Lemma opp_karoubi_counit_is_inverse
+      : is_inverse_in_precat (adjcounit opp_karoubi_is_adjoint c) opp_karoubi_counit_iso_inv.
+    Proof.
+      split.
+      - apply karoubi_mor_eq.
+        exact (pr22 c).
+      - apply karoubi_mor_eq.
+        exact (pr22 c).
+    Qed.
+
+    Definition opp_karoubi_counit_is_iso
+      : is_z_isomorphism (adjcounit opp_karoubi_is_adjoint c)
+      := make_is_z_isomorphism _ _ opp_karoubi_counit_is_inverse.
+
+  End OppKaroubiCounitIso.
+
+  Definition opp_karoubi
+    : adj_equiv (op_cat (karoubi_envelope C)) (karoubi_envelope (op_cat C))
+    := opp_karoubi_functor ,,
+      opp_karoubi_is_adjoint ,,
+      opp_karoubi_unit_is_iso ,,
+      opp_karoubi_counit_is_iso.
+
+End OppKaroubiEquiv.
