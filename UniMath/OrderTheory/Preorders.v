c387f733aee03c901307b69f35a26dafd1da3367
Tempestas Ludi <tempestasludi@gmail.com>
2024-10-16 11:15:29 +0200

    Add the downward closure of a preorder, split up the code about "downwardly closed down types" into down types and downwardly closed subtypes

--- UniMath/OrderTheory/Preorders.v
+++ UniMath/OrderTheory/Preorders.v
@@ -1,65 +1,105 @@
 Require Import UniMath.Foundations.All.
 Require Import UniMath.MoreFoundations.All.
 
-Section DownTypes.
-
-  Definition down_type
-    {X : UU}
-    (P : po X)
-    (x : X)
-    : UU
-    := ∑ y, P y x.
-
-  Definition down_type_element
-    {X : UU}
-    {P : po X}
-    {x : X}
-    (y : down_type P x)
-    : X
-    := pr1 y.
-
-  Definition down_type_is_down
-    {X : UU}
-    {P : po X}
-    {x : X}
-    (y : down_type P x)
-    : P (down_type_element y) x
-    := pr2 y.
-
-End DownTypes.
-
-Section DownwardClosedDownSubtype.
+Section SubPreorder.
 
   Context {X : UU}.
   Context (P : po X).
-  Context (x : X).
+  Context (A : hsubtype X).
+
+  Definition subpreorder
+    : po A.
+  Proof.
+    use make_po.
+    - exact (resrel P A).
+    - abstract (
+        split;
+        [ apply istransresrel;
+          apply istrans_po
+        | apply isreflresrel;
+          apply isrefl_po ]
+      ).
+  Defined.
+
+End SubPreorder.
+
+Section DownType.
+
+  Definition down_type
+    {X : UU}
+    (P : hrel X)
+    (y : X)
+    : hsubtype X
+    := λ x, P x y.
+
+  Definition up_type
+    {X : UU}
+    (P : hrel X)
+    (x : X)
+    : hsubtype X
+    := λ y, P x y.
+
+End DownType.
+
+Section DownwardClosed.
+
+  Context {X : UU}.
+  Context (P : hrel X).
 
   Definition is_downward_closed
-    (f : hsubtype (down_type P x))
+    (f : hsubtype X)
     : UU
-    := ∏ (y : f)
-      (z : X)
-      (Hzy : P z (down_type_element (pr1carrier _ y))),
-      f (z ,, istrans_po P _ _ _ Hzy (down_type_is_down (pr1carrier _ y))).
+    := ∏ (x : f)
+      (y : down_type P (pr1carrier _ x)),
+      f (pr1carrier _ y).
 
-  Definition downward_closed_down_subtype
+  Definition downward_closed_subtype
     : UU
     := ∑ f, is_downward_closed f.
 
-  Coercion downward_closed_down_subtype_to_subtype
-    (f : downward_closed_down_subtype)
-    : hsubtype (down_type P x)
+  Coercion downward_closed_subtype_to_subtype
+    (f : downward_closed_subtype)
+    : hsubtype X
     := pr1 f.
 
   Definition downward_closed_is_downward_closed
-    (f : downward_closed_down_subtype)
+    (f : downward_closed_subtype)
     : is_downward_closed f
     := pr2 f.
 
-  Definition make_downward_closed_down_subtype
-    (f : hsubtype (down_type P x))
+  Definition make_downward_closed_subtype
+    (f : hsubtype X)
     (H : is_downward_closed f)
-    : downward_closed_down_subtype
+    : downward_closed_subtype
     := f ,, H.
 
-End DownwardClosedDownSubtype.
+End DownwardClosed.
+
+Section DownwardClosure.
+
+  Context {X : UU}.
+  Context (P : po X).
+  Context (f : hsubtype X).
+
+  Definition downward_closure_hsubtype
+    : hsubtype X.
+  Proof.
+    intro y.
+    refine (∃ (z : f), P y (pr1carrier _ z)).
+  Defined.
+
+  Lemma downward_closure_is_downward_closed
+    : is_downward_closed P downward_closure_hsubtype.
+  Proof.
+    intros y z.
+    use (hinhfun _ (pr2 y)).
+    intro Hy.
+    exists (pr1 Hy).
+    refine (istrans_po P _ _ _ (pr2 z) (pr2 Hy)).
+  Qed.
+
+  Definition downward_closure
+    : downward_closed_subtype P
+    := make_downward_closed_subtype P _ downward_closure_is_downward_closed.
+
+End DownwardClosure.
