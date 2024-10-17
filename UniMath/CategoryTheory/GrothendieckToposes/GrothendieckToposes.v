307da410e121461f8b1c60baec39b79730ec845b
Tempestas Ludi <tempestasludi@gmail.com>
2024-10-17 09:28:39 +0200

    Improve packaging of GrothendieckToposes, make compatible again with changes elsewhere

--- UniMath/CategoryTheory/GrothendieckToposes/GrothendieckToposes.v
+++ UniMath/CategoryTheory/GrothendieckToposes/GrothendieckToposes.v
@@ -22,22 +22,19 @@ Section def_grothendiecktopos.
   (** Here (pr1 D) is the precategory which is equivalent to the precategory of sheaves on the
       Grothendieck topology (pr2 D). *)
   Definition Grothendieck_topos : UU :=
-    ∑ D' : (∑ D : category × (Grothendieck_topology C),
-                  functor (pr1 D) (sheaf_cat C (pr2 D))),
-           (adj_equivalence_of_cats (pr2 D')).
+    ∑ D : category × Grothendieck_topology C,
+      adj_equiv (pr1 D) (sheaf_cat (pr2 D)).
 
   (** Accessor functions *)
-  Definition Grothendieck_topos_category (GT : Grothendieck_topos) : category :=
-    pr1 (pr1 (pr1 GT)).
-  Coercion Grothendieck_topos_category : Grothendieck_topos >-> category.
+  Coercion Grothendieck_topos_category (GT : Grothendieck_topos) : category :=
+    pr11 GT.
 
   Definition Grothendieck_topos_Grothendieck_topology (GT : Grothendieck_topos) :
-    Grothendieck_topology C := pr2 (pr1 (pr1 GT)).
+    Grothendieck_topology C := pr21 GT.
 
   Definition Grothendieck_topos_functor (GT : Grothendieck_topos) :
-    functor (Grothendieck_topos_category GT)
-            (sheaf_cat C (Grothendieck_topos_Grothendieck_topology GT)) :=
-    pr2 (pr1 GT).
+    functor GT (sheaf_cat (Grothendieck_topos_Grothendieck_topology GT)) :=
+    pr2 GT.
 
   Definition Grothendieck_topos_equivalence (GT : Grothendieck_topos) :
     adj_equivalence_of_cats (Grothendieck_topos_functor GT) := pr2 GT.
